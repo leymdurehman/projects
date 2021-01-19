7a0db70990fd87455aa7a2d053f2c615b9b12b85
Leymaan Abdurehman <Leymdurehman@gmail.com>
2021-01-19 02:07:20 -0500

    Linear Conversion

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,53 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+
+				
+				
+				Scanner input = new Scanner( System.in );
+				
+				
+				System.out.print("Please enter the length:");
+				String numInput = input.nextLine();
+				double numAmount = Double.parseDouble(numInput);
+				
+				System.out.print("Is the measurement in (m)eter, or (f)eet?");
+				String measureInput = input.nextLine();
+				
+					if ( measureInput.equals("f")) {
+						
+						double meters = (numAmount * 0.3048);
+						
+						int metersFinal = (int) meters;
+						int feetFinal = (int) numAmount;
+						
+							System.out.printf( feetFinal + "f is " + metersFinal + "m."); }
+					
+				   else if  ( measureInput.equals("m")) {
+						
+						double feet = (numAmount * 3.2808399);
+						
+						Math.round(feet); // rounds to nearest integer
+						
+						int feetFinal = (int) feet;
+						int metersFinal = (int) numAmount;
+						
+						
+						
+							System.out.printf( metersFinal + " m is " + feetFinal + "f.");
+					}
+				
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
