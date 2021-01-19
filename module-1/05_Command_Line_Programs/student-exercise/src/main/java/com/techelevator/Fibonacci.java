34f9075587da6eedb89b894b4f1ae91900efb5d3
Leymaan Abdurehman <Leymdurehman@gmail.com>
2021-01-18 23:46:50 -0500

    Fibonacci ex

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 	
 	
@@ -8,7 +10,56 @@ public class Fibonacci {
 	
 
 	public static void main(String[] args) {
-
-	}
-
+		
+		
+		
+		
+	 int fibNumMax; 
+	 int num1 = 0;
+	 int num2 = 1;
+		 
+		 
+		
+	       
+	        
+	        Scanner input = new Scanner(System.in);
+	        
+	        System.out.println("Enter the Fibonacci Number:");
+	        
+	        fibNumMax = input.nextInt();
+	        
+	        int i=1;
+	 	       while(i <= fibNumMax && num1 < fibNumMax)
+	 	      {
+	 	          System.out.print(num1+" ");
+	 	          
+	 	          int sumOfPrevTwo = num1 + num2;
+	 	          
+	 	          num1 = num2;
+	 	          
+	 	         num2 = sumOfPrevTwo;
+	 	         
+	 	           i++;
+	 	           if ( num1 == fibNumMax) {
+	 	        	   
+	 	        	   break;
+	 	        	  
+	 	           }
+	 	       }
+	 	    }
 }
+	        
+	        
+	    
+
+	  //      int i=1;
+	   //     while(i<=count)
+	  //      {
+	  //          System.out.print(num1+" ");
+	  //          int sumOfPrevTwo = num1 + num2;
+	   //         num1 = num2;
+	   //         num2 = sumOfPrevTwo;
+	   //         i++;
+	//        }
+	    
+	
