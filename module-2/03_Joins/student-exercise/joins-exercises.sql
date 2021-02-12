5143a7487e759f131fe6b8a205e22c63bb295993
Leymaan Abdurehman <Leymdurehman@gmail.com>
2021-02-11 23:46:47 -0500

    push
    git status
    :wq
    :wq

--- module-2/03_Joins/student-exercise/joins-exercises.sql
+++ module-2/03_Joins/student-exercise/joins-exercises.sql
@@ -12,41 +12,138 @@ WHERE actor.first_name = 'NICK' AND actor.last_name = 'STALLONE';
 -- 2. All of the films that Rita Reynolds has appeared in
 -- (20 rows)
 
+
+SELECT film.title
+FROM actor
+JOIN film_actor ON film_actor.actor_id = actor.actor_id
+Join film ON film_actor.film_id = film.film_id
+WHERE actor.first_name = 'RITA' AND actor.last_name = 'REYNOLDS';
+
+
+
+
 -- 3. All of the films that Judy Dean or River Dean have appeared in
 -- (46 rows)
+
+
+SELECT film.title
+FROM actor
+JOIN film_actor ON film_actor.actor_id = actor.actor_id
+Join film ON film_actor.film_id = film.film_id
+WHERE (actor.first_name = 'JUDY' AND actor.last_name = 'DEAN') OR (actor.first_name = 'RIVER' AND actor.last_name = 'DEAN');
 
 -- 4. All of the the ‘Documentary’ films
 -- (68 rows)
 
+SELECT film.title, category.name        // I did the C.R.U.D. HW first which affected my amt of return rows
+FROM category
+JOIN film_category ON film_category.category_id = category.category_id
+JOIN film ON film.film_id = film_category.film_id
+WHERE category.name = 'Documentary';
+
+
+
+
 -- 5. All of the ‘Comedy’ films
 -- (58 rows)
+
+SELECT film.title, category.name
+FROM category
+JOIN film_category ON film_category.category_id = category.category_id
+JOIN film ON film.film_id = film_category.film_id
+WHERE category.name = 'Comedy';
+
 
 -- 6. All of the ‘Children’ films that are rated ‘G’
 -- (10 rows)
 
+SELECT film.title, category.name
+FROM category
+JOIN film_category ON film_category.category_id = category.category_id
+JOIN film ON film.film_id = film_category.film_id
+WHERE category.name = 'Children' AND film.rating = 'G';
+
+
 -- 7. All of the ‘Family’ films that are rated ‘G’ and are less than 2 hours in length
 -- (3 rows)
 
+
+SELECT film.title, category.name
+FROM category
+JOIN film_category ON film_category.category_id = category.category_id
+JOIN film ON film.film_id = film_category.film_id
+WHERE category.name = 'Family' AND film.rating = 'G' AND film.length < 120;
+
+
 -- 8. All of the films featuring actor Matthew Leigh that are rated ‘G’
 -- (9 rows)
+SELECT film.title
+FROM actor
+JOIN film_actor ON film_actor.actor_id = actor.actor_id
+Join film ON film_actor.film_id = film.film_id
+WHERE actor.first_name = 'MATTHEW' AND actor.last_name = 'LEIGH' and film.rating = 'G' ;
+
 
 -- 9. All of the ‘Sci-Fi’ films released in 2006
 -- (61 rows)
 
+
+SELECT film.title, category.name, release_year
+FROM category
+JOIN film_category ON film_category.category_id = category.category_id
+JOIN film ON film.film_id = film_category.film_id
+WHERE release_year = 2006 AND name = 'Sci-Fi';
+
+
+
+
 -- 10. All of the ‘Action’ films starring Nick Stallone
 -- (2 rows)
 
+SELECT *
+FROM actor
+JOIN film_actor ON film_actor.actor_id = actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON category.category_id = film_category.category_id
+WHERE actor.first_name = 'NICK' AND actor.last_name = 'STALLONE' AND category.name = 'Action';
+
+
+
+
 -- 11. The address of all stores, including street address, city, district, and country
 -- (2 rows)
+SELECT address, city, district, country
+FROM store
+JOIN address ON address.address_id = store.address_id
+JOIN city ON address.city_id = city.city_id
+JOIN country ON country.country_id = city.country_id;
 
 -- 12. A list of all stores by ID, the store’s street address, and the name of the store’s manager
 -- (2 rows)
+SELECT store_id, address, manager_staff_id
+FROM store
+JOIN address ON address.address_id = store.address_id;
 
 -- 13. The first and last name of the top ten customers ranked by number of rentals 
 -- (#1 should be “ELEANOR HUNT” with 46 rentals, #10 should have 39 rentals)
+SELECT first_name, last_name, COUNT(rental.customer_id)
+FROM rental
+JOIN customer ON customer.customer_id = rental.customer_id
+GROUP BY customer.first_name, customer.last_name
+ORDER BY count DESC LIMIT 10;
+
+
 
 -- 14. The first and last name of the top ten customers ranked by dollars spent 
 -- (#1 should be “KARL SEAL” with 221.55 spent, #10 should be “ANA BRADLEY” with 174.66 spent)
+
+SELECT first_name, last_name, SUM(payment.amount)
+FROM payment
+JOIN customer ON customer.customer_id = payment.customer_id
+GROUP BY customer.first_name, customer.last_name
+ORDER BY SUM DESC LIMIT 10;
+ 
 
 -- 15. The store ID, street address, total number of rentals, total amount of sales (i.e. payments), and average sale of each store.
 -- (NOTE: Keep in mind that an employee may work at multiple stores.)
@@ -66,14 +163,75 @@ GROUP BY store.store_id, address.address;
 -- 16. The top ten film titles by number of rentals
 -- (#1 should be “BUCKET BROTHERHOOD” with 34 rentals and #10 should have 31 rentals)
 
+SELECT film.title, COUNT(rental.customer_id)
+FROM rental
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN film ON inventory.film_id = film.film_id
+GROUP BY film.title
+ORDER BY count DESC LIMIT 10;
+
+
+
+
+
 -- 17. The top five film categories by number of rentals 
 -- (#1 should be “Sports” with 1179 rentals and #5 should be “Family” with 1096 rentals)
+
+SELECT category.name, COUNT(rental.customer_id)
+FROM rental
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN film ON inventory.film_id = film.film_id
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON category.category_id = film_category.film_id
+GROUP BY category.name 
+ORDER BY COUNT DESC LIMIT 5;
+
+
+
 
 -- 18. The top five Action film titles by number of rentals 
 -- (#1 should have 30 rentals and #5 should have 28 rentals)
 
+SELECT film.title, category.name, COUNT(rental.customer_id)
+FROM rental
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN film ON inventory.film_id = film.film_id
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON category.category_id = film_category.category_id
+WHERE category.name = 'Action'
+GROUP BY film.title, category.name
+ORDER BY COUNT DESC LIMIT 5;
+
+
+
 -- 19. The top 10 actors ranked by number of rentals of films starring that actor 
 -- (#1 should be “GINA DEGENERES” with 753 rentals and #10 should be “SEAN GUINESS” with 599 rentals)
+ -- actor iD to film id to inventory -filmid to inventory-id to rental inventory id -call rental.customerid
+ 
+SELECT actor.first_name, actor.last_name, actor.actor_id, count(rental.customer_id)
+FROM actor
+JOIN film_actor ON film_actor.actor_id = actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+JOIN inventory ON inventory.film_id = film.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+GROUP BY actor.actor_id
+ORDER BY COUNT DESC LIMIT 10;
+
+
+
+
 
 -- 20. The top 5 “Comedy” actors ranked by number of rentals of films in the “Comedy” category starring that actor 
 -- (#1 should have 87 rentals and #5 should have 72 rentals)
+
+SELECT actor.first_name, actor.last_name, actor.actor_id, count(rental.customer_id)
+FROM actor
+JOIN film_actor ON film_actor.actor_id = actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON category.category_id = film_category.category_id
+JOIN inventory ON inventory.film_id = film.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+WHERE category.name = 'Comedy'
+GROUP BY actor.actor_id
+ORDER BY COUNT DESC LIMIT 5;
--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -3,34 +3,88 @@
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
 
+SELECT * FROM actor;
+
+INSERT INTO actor(first_name,last_name)
+VALUES ('HAMPTON', 'AVENUE');
+INSERT INTO actor(first_name,last_name)
+VALUES ('LISA', 'BYWAY');
+
+
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
 
+SELECT * FROM film;
+INSERT INTO film (title, description, release_year, language_id, length)
+VALUES ('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', '2008', '1' , '198');
+
+
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
 
+
+-- this question kind of confused me -- didnt know what 'add them' meant so I updated the description to have their names/roles
+UPDATE film
+SET description = 'The epic story of Euclid as a pizza delivery boy in
+-- ancient Greece. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
+-- overprotective mother'
+WHERE film_id = 1001;
+
+
 -- 4. Add Mathmagical to the category table.
+
+INSERT INTO category VALUES ('17', 'Mathemagical');
+SELECT * FROM category;
+
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+
+UPDATE film_category
+SET category_id = '17' WHERE film_id = ('996' :: INTEGER);
+UPDATE film_category
+SET category_id = '17' WHERE film_id = ('276' :: INTEGER); 
+UPDATE film_category
+SET category_id = '17' WHERE film_id = ('494' :: INTEGER); 
+INSERT INTO film_category
+VALUES ('1001', '17');
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
 
+
+UPDATE film
+SET rating = ('G')
+WHERE title = 'Euclidean PI' 
+or title = 'RANDOM GO' or title = 'EGG IGBY' 
+or title = 'KARATE MOON 'or title = 'RANDOM GO' or title ='YOUNG LANGUAGE';
+
+
+
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+
+INSERT INTO inventory (film_id, store_id)
+VALUES (1001, 1 ), (1001, 2);
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+
+DELETE FROM film WHERE title = 'Euclidean PI';
+
+-- <** NO- did not work due to foreign key constraint-this film is being referenced multiple different places>
 
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+
+DELETE FROM category WHERE name = 'Mathemagical'; //**I spelled it Math(e)magical orginally so I had to do it here too
+
+
+-- <** NO - did not work because this category is being referenced in another table (film_category) >
 
 -- 10. Delete all links to Mathmagical in the film_category tale.
 -- (Did it succeed? Why?)
