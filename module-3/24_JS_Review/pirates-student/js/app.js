09b60f241ec93f4cead216d0ffccde876e01fdd1
Leymaan Abdurehman <Leymdurehman@gmail.com>
2021-03-21 22:04:17 -0400

    notes

--- module-3/24_JS_Review/pirates-student/js/app.js
+++ module-3/24_JS_Review/pirates-student/js/app.js
@@ -3,7 +3,43 @@
 */
 
 // Creates the Grid -- this should only occur after the DOM loads
+
+document.addEventListener('')
+
 createGrid();
+document.querySelector('body').addEventListener('keydown', (event) =>{
+
+
+
+}
+// isLoss func - return with a logical opperatior
+
+
+);
+
+
+function moveShipRight(){
+    const ship = getShipLocation();
+    const right = ship.nextElementSibling;
+    moveShip(ship,right);
+ 
+
+}
+
+function moveShipDown () {
+
+    const ship = getShipLocation();
+    const rowBelow = ship.parentElement.nextElementSibling
+}
+
+
+
+// function move ship (oldLocation, newLocation)
+
+
+//function get ship location
+
+
 
 
 /**
@@ -12,11 +48,12 @@ createGrid();
 function resetGame() {
 
     // Create the Obstacles
- 
+    createObstacles();
     // Inform the user they can start
 
    // Set the starting location of the boat and treasure
-
+    const frame = document.getElementById('frame');
+    frame.firstElementChild.firstElementChild.classList.add('boat');
 }
 
 
@@ -39,7 +76,9 @@ function createGrid() {
  * @param {HTMLElement} frame 
  */
 function buildRow(frame) {
-    const row = null; // create the row and add it to the frame
+    const row = document.createElement('div');
+    row.classList.add('row');
+    frame.insertAdjacentElement('beforeend',row);
     for (let i = 0; i < 10 ; i++) {
         buildSquare(row, i); 
     }    
@@ -52,10 +91,33 @@ function buildRow(frame) {
  */
 function buildSquare(row, count) {
    // Add code to create the grid Squares and add them to the row
-    
+const cell = document.createElement('div');
+    cell.classList.add('');    
 }
 
+function createObstacles(){
 
+
+
+// loop over all the cells in all the rows
+// for each cell call addObstacles(cell)
+// only if cell is not boat or treasure  
+
+
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((row, rowIndex) => {
+        const cell = row.children;
+        Array.from(cells).forEach(cell, cellIndex) => {
+            if(!(rowIndex ==0 && cellIndex ==0) && !(rowIndex == rows.length -1 && cellIndex == cells.length -1)) {
+                addObstacles(cell);
+
+            };
+            
+        };
+
+
+    });
+}
 
 
 /**
@@ -64,14 +126,19 @@ function buildSquare(row, count) {
  * @param {HTMLElement} cell 
  */
 function addObstacles(cell) {
+
     // remove any existing pirates or icebergs
- 
+    cell.classList.remove('pirate');
+    cell.classList.remove('iceberg');
+
     const rand = Math.floor(Math.random() * 100) + 1;
 
     if (rand > 85) {
         // Add pirates here
+        cell.classList.add('pirate');
     } else if (rand > 80) {
         // Add iceberg here
+        cell.classList.add('iceberg');
     } 
 }
 
